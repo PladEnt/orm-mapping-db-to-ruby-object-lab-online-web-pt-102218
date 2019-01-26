55bd8fcef53868274bc89f666d17052eca890a92
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-01-26 16:47:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,15 +51,7 @@ class Student
   end
   
   def self.all_students_in_grade_X(grade)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = ?
-      ORDER BY students.id
-    SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    
   end
   
 
