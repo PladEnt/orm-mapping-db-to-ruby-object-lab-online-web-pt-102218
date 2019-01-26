73b9f70945560616d5d43528d020df242a251821
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-01-26 16:38:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,8 +40,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade=10
-      WHERE id=1
+      WHERE grade=10 && id=1
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
