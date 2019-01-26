e41e91b602ca09e15cbb7fbf253aa2304df8640f
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-01-26 16:47:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,7 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(num)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT *
       FROM students
