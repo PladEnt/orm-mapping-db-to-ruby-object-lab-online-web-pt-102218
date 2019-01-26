72b2196079ad703fcea0c047fad6487dfc9c8951
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-01-26 16:38:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ class Student
       SELECT *
       FROM students
       WHERE grade=10
-      LIMIT 1
+      WHERE id=1
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
